fa292f0fc3428b84f70bb582dba42b8fb9bff559
Kyla Iglesia <kyla.iglesia@gmail.com>
2022-01-10 17:23:03 +0800

    Initial commit - barebones Flask app

---
+++ flex_bootcamp_project_1/__init__.py
@@ -0,0 +1,6 @@
+import os
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='flex_bootcamp_project_1',
+    packages=['flex_bootcamp_project_1'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'twilio'
+    ]
+)
